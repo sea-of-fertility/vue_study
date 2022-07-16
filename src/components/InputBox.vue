9be973e60e60751390e561f821605676171654af
sujin <atena9604@naver.com>
2022-07-17 01:46:54 +0900

    [feat] basic front end logic

---
+++ src/components/InputBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <textarea v-model="item"/> <button @click="onSave()">저장</button>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+  export default {
+    name: 'InputBox',
+    data() {
+      return{
+        item: "",
+    }
+  },
+    methods: {
+      onSave() {
+        axios.put('http://localhost:8081/saveList', {newContent: this.item})
+            .then(res => {
+              console.log(res.data)
+            })
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <InputBox/>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import InputBox from '@/components/InputBox.vue'
+import axios from "axios";
+
+export default {
+  name: 'HomeView',
+  data (){
+    return{
+      items : [],
+    }
+  },
+  components: {
+    InputBox
+  },
+  methods: {
+    computed() {
+      axios.get('http://localhost:8081/getList')
+          .then(res => {
+            console.log(res.data)
+          })
+    }
+  }
+
+}
+</script>
